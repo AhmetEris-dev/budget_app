4ab7af3d688ee7494a84e0f6065856c78a256ea3
AhmetEris-dev <ahmet2_lock@hotmail.com>
2026-02-09 12:03:44 +0300

    deneme

--- src/main/java/com/ahmete/budget_app/BudgetAppApplication.java
+++ src/main/java/com/ahmete/budget_app/BudgetAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BudgetAppApplication {
 	
+	// deneme
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetAppApplication.class, args);
 	}
